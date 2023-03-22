f52dcc382c4edb3c2e6dc4e599e1cf6f7dbc3ed3
Linda <linda.bertling@utb.ecutbildning.se>
2023-03-22 08:54:12 +0100

    styling added to videoview

--- src/views/EmbeddedVideoView.js
+++ src/views/EmbeddedVideoView.js
@@ -1,9 +1,10 @@
 import React from "react";
 import YoutubeEmbed from "../components/youtubeEmbed/YoutubeEmbed";
+import "./EmbeddedVideoView.css";
 
 const EmbeddedVideoView = () => {
   return (
-    <div>
+    <div className="main-content-container">
       <YoutubeEmbed embedId={"IHL9u3uN65Q"} />
     </div>
   );
